834af4e9735b9af1f5deaafdd7e850c43fdd8325
Navindi-Thisara <navindithisara214@gmail.com>
2025-08-26 19:52:21 -0300

    Updated footer content

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+  </main>
+
+  <footer class="footer">
+    <div class="container">
+      <p>&copy; <?php echo date("Y"); ?> Food Ordering System. All rights reserved.</p>
+    </div>
+  </footer>
+
+</body>
+</html>
